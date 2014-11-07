33c690a0f2145ea5c9d00e09c3be7a855b5a30a7
David Guttman <david@davidguttman.com>
2014-11-07 11:24:22 -0800

    fixed tests

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,4 @@
 var test = require('tape')
-var assert = require('assert')
-
-var sassify = require('..')
 
 test("basic", function (t) {
   var el = document.createElement('div')
@@ -10,14 +7,14 @@ test("basic", function (t) {
   document.body.appendChild(el)
 
   var style1 = window.getComputedStyle(el)
-  assert.equal(style1['background-color'], 'rgba(0, 0, 0, 0)')
-  assert.equal(style1.color, 'rgb(0, 0, 0)')
+  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)')
+  t.equal(style1.color, 'rgb(0, 0, 0)')
 
   require('./basic.scss')
 
   var style2 = window.getComputedStyle(el)
-  assert.equal(style2['background-color'], 'rgb(128, 0, 128)')
-  assert.equal(style2.color, 'rgb(255, 255, 0)')
+  t.equal(style2['background-color'], 'rgb(128, 0, 128)')
+  t.equal(style2.color, 'rgb(255, 255, 0)')
 
   t.end()
 })
