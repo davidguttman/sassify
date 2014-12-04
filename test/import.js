2c2995c0ece0208346a560755dac058de64a7e84
Adriaan Callaerts <adriaan.callaerts@gmail.com>
2014-12-04 12:28:53 +0100

    Fixed other test

--- test/import.js
+++ test/import.js
@@ -1,5 +1,4 @@
-var test = require('tape')
-var assert = require('assert')
+var test = require('tape');
 
 test("import", function (t) {
   var el = document.createElement('div')
@@ -8,13 +7,13 @@ test("import", function (t) {
   document.body.appendChild(el)
 
   var style1 = window.getComputedStyle(el)
-  assert.equal(style1['background-color'], 'rgba(0, 0, 0, 0)')
-  assert.equal(style1.color, 'rgb(0, 0, 0)')
+  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)')
+  t.equal(style1.color, 'rgb(0, 0, 0)')
 
   require('./import.scss')
 
   var style2 = window.getComputedStyle(el)
-  assert.equal(style2.color, 'rgb(255, 192, 203)')
+  t.equal(style2.color, 'rgb(255, 192, 203)')
 
   t.end()
 })
