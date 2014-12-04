fd1155dcb1db463a5dbc4e8c46f85e700d8dfcf5
Adriaan Callaerts <adriaan.callaerts@gmail.com>
2014-12-04 14:27:02 +0100

    Fixed import test and added missing semicolons

--- test/basic.js
+++ test/basic.js
@@ -1,20 +1,20 @@
-var test = require('tape')
+var test = require('tape');
 
 test("basic", function (t) {
-  var el = document.createElement('div')
-  el.classList.add('some-div')
-  el.innerHTML = 'some text'
-  document.body.appendChild(el)
+  var el = document.createElement('div');
+  el.classList.add('some-div');
+  el.innerHTML = 'some text';
+  document.body.appendChild(el);
 
-  var style1 = window.getComputedStyle(el)
-  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)')
-  t.equal(style1.color, 'rgb(0, 0, 0)')
+  var style1 = window.getComputedStyle(el);
+  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)');
+  t.equal(style1.color, 'rgb(0, 0, 0)');
 
-  require('./basic.scss')
+  require('./basic.scss');
 
-  var style2 = window.getComputedStyle(el)
-  t.equal(style2['background-color'], 'rgb(128, 0, 128)')
-  t.equal(style2.color, 'rgb(255, 255, 0)')
+  var style2 = window.getComputedStyle(el);
+  t.equal(style2['background-color'], 'rgb(128, 0, 128)');
+  t.equal(style2.color, 'rgb(255, 255, 0)');
 
-  t.end()
+  t.end();
 })
--- test/import.js
+++ test/import.js
@@ -1,19 +1,19 @@
 var test = require('tape');
 
 test("import", function (t) {
-  var el = document.createElement('div')
-  el.classList.add('import')
-  el.innerHTML = 'import text'
-  document.body.appendChild(el)
+  var el = document.createElement('div');
+  el.classList.add('import');
+  el.innerHTML = 'import text';
+  document.body.appendChild(el);
 
-  var style1 = window.getComputedStyle(el)
-  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)')
-  t.equal(style1.color, 'rgb(0, 0, 0)')
+  var style1 = window.getComputedStyle(el);
+  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)');
+  t.equal(style1.color, 'rgb(0, 0, 0)');
 
-  require('./import.scss')
+  require('./import.scss');
 
-  var style2 = window.getComputedStyle(el)
-  t.equal(style2.color, 'rgb(255, 192, 203)')
+  var style2 = window.getComputedStyle(el);
+  t.equal(style2.color, 'rgb(255, 192, 203)');
 
   t.end()
 })
