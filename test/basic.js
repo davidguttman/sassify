e4c7858691d2439f64d66bde5e51ebd5f3f16648
Adriaan Callaerts <adriaan.callaerts@gmail.com>
2014-12-05 11:27:01 +0100

    fine-tuned tests so they would run on travis using both phantom and firefox

--- test/basic.js
+++ test/basic.js
@@ -6,15 +6,15 @@ test("basic", function (t) {
   el.innerHTML = 'some text';
   document.body.appendChild(el);
 
-  var style1 = window.getComputedStyle(el);
-  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)');
-  t.equal(style1.color, 'rgb(0, 0, 0)');
+  var style1 = window.getComputedStyle(el, null);
+  t.ok(['rgba(0, 0, 0, 0)', 'transparent'].indexOf(style1['background-color'] || style1.backgroundColor) !== -1, 'default background-color should be transparent white or transparent');
+  t.equal(style1.color, 'rgb(0, 0, 0)', 'default color should be black');
 
   require('./basic.scss');
 
-  var style2 = window.getComputedStyle(el);
-  t.equal(style2['background-color'], 'rgb(128, 0, 128)');
-  t.equal(style2.color, 'rgb(255, 255, 0)');
+  var style2 = window.getComputedStyle(el, null);
+  t.equal(style2['background-color'] || style2.backgroundColor, 'rgb(128, 0, 128)', 'imported background-color should be applied');
+  t.equal(style2.color, 'rgb(255, 255, 0)', 'imported color should be applied');
 
   t.end();
 })
--- test/import.js
+++ test/import.js
@@ -7,13 +7,13 @@ test("import", function (t) {
   document.body.appendChild(el);
 
   var style1 = window.getComputedStyle(el);
-  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)');
-  t.equal(style1.color, 'rgb(0, 0, 0)');
+  t.ok(['rgba(0, 0, 0, 0)', 'transparent'].indexOf(style1['background-color'] || style1.backgroundColor) !== -1, 'default background-color should be transparent white or transparent');
+  t.equal(style1.color, 'rgb(0, 0, 0)', 'default color should be black');
 
   require('./import.scss');
 
   var style2 = window.getComputedStyle(el);
-  t.equal(style2.color, 'rgb(255, 192, 203)');
+  t.equal(style2.color, 'rgb(255, 192, 203)', 'recursively imported color should be applied');
 
   t.end()
 })
