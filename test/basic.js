b29852c0d01d40e43c51ad994a3bcd7ed4955591
Adriaan Callaerts <adriaan.callaerts@gmail.com>
2015-01-13 14:17:09 +0100

    Merging some stuff from other forks

--- test/basic.js
+++ test/basic.js
@@ -1,20 +1,29 @@
 var test = require('tape');
 
 test("basic", function (t) {
+  t.plan(4);
+  var body = document.body;
+
   var el = document.createElement('div');
-  el.classList.add('some-div');
+  el.classList.add('basic');
   el.innerHTML = 'some text';
-  document.body.appendChild(el);
+  body.appendChild(el);
 
   var style1 = window.getComputedStyle(el, null);
   t.ok(['rgba(0, 0, 0, 0)', 'transparent'].indexOf(style1['background-color'] || style1.backgroundColor) !== -1, 'default background-color should be transparent white or transparent');
   t.equal(style1.color, 'rgb(0, 0, 0)', 'default color should be black');
 
-  require('./basic.scss');
-
-  var style2 = window.getComputedStyle(el, null);
-  t.equal(style2['background-color'] || style2.backgroundColor, 'rgb(128, 0, 128)', 'imported background-color should be applied');
-  t.equal(style2.color, 'rgb(255, 255, 0)', 'imported color should be applied');
-
-  t.end();
+  var link = require('./basic.scss');
+  link.onload = function(e) {
+	window.clearTimeout(timer);
+    var style2 = window.getComputedStyle(el, null);
+    t.equal(style2['background-color'] || style2.backgroundColor, 'rgb(128, 0, 128)', 'imported background-color should be applied');
+    t.equal(style2.color, 'rgb(255, 255, 0)', 'imported color should be applied');
+  };
+  link.onerror = function(e) {
+    t.fail(e.message);
+    t.end();
+  };
+  
+  var timer = window.setTimeout(link.onload, 1000);
 })
--- test/import.js
+++ test/import.js
@@ -1,19 +1,28 @@
 var test = require('tape');
 
 test("import", function (t) {
+  t.plan(3);
+  var body = document.body;
+
   var el = document.createElement('div');
   el.classList.add('import');
   el.innerHTML = 'import text';
-  document.body.appendChild(el);
+  body.appendChild(el);
 
   var style1 = window.getComputedStyle(el);
   t.ok(['rgba(0, 0, 0, 0)', 'transparent'].indexOf(style1['background-color'] || style1.backgroundColor) !== -1, 'default background-color should be transparent white or transparent');
   t.equal(style1.color, 'rgb(0, 0, 0)', 'default color should be black');
 
-  require('./import.scss');
-
-  var style2 = window.getComputedStyle(el);
-  t.equal(style2.color, 'rgb(255, 192, 203)', 'recursively imported color should be applied');
-
-  t.end()
+  var link = require('./import.scss');
+  link.onload = function(e) {
+	window.clearTimeout(timer);
+    var style2 = window.getComputedStyle(el);
+    t.equal(style2.color, 'rgb(255, 192, 203)', 'recursively imported color should be applied');
+  };
+  link.onerror = function(e) {
+    t.fail(e.message);
+    t.end();
+  };
+  
+  var timer = window.setTimeout(link.onload, 1000);
 })
