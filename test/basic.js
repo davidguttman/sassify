1610e0ececa06eee7472a061fb763d2a8863f0fd
David Guttman <david@davidguttman.com>
2016-02-29 08:35:36 -0800

    updated sass, fixed tests

--- test/basic.js
+++ test/basic.js
@@ -6,15 +6,17 @@ test("basic", function (t) {
   el.innerHTML = 'some text'
   document.body.appendChild(el)
 
-  var style1 = window.getComputedStyle(el)
-  t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)')
-  t.equal(style1.color, 'rgb(0, 0, 0)')
+  setTimeout(function () {
+    var style1 = window.getComputedStyle(el)
+    t.equal(style1['background-color'], 'rgba(0, 0, 0, 0)')
+    t.equal(style1.color, 'rgb(0, 0, 0)')
 
-  require('./basic.scss')
+    require('./basic.scss')
 
-  var style2 = window.getComputedStyle(el)
-  t.equal(style2['background-color'], 'rgb(128, 0, 128)')
-  t.equal(style2.color, 'rgb(255, 255, 0)')
+    var style2 = window.getComputedStyle(el)
+    t.equal(style2['background-color'], 'rgb(128, 0, 128)')
+    t.equal(style2.color, 'rgb(255, 255, 0)')
 
-  t.end()
+    t.end()
+  }, 50)
 })
